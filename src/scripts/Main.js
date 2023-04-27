0bbbd1f38cc1813d4701ab5a874734f44af71b35
Matthieu Parent <matthieuparent@outlook.com>
2023-04-27 11:02:34 -0400

    updated template strucutre

---
+++ src/scripts/Main.js
@@ -0,0 +1,13 @@
+import Icons from './utils/Icons';
+
+class Main {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    document.documentElement.classList.add('has-js');
+    Icons.load();
+  }
+}
+new Main();
